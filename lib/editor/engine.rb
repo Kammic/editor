eed4c33f2c6a28d61488fbaca86249c185619fff
Sumeet Singh <ortuna@gmail.com>
2013-11-06 23:00:54 -0600

    Added editor.css and editor.js to precompile

--- lib/editor/engine.rb
+++ lib/editor/engine.rb
@@ -1,5 +1,7 @@
 module Editor
   class Engine < ::Rails::Engine
-
+    initializer "editor.assets.precompile" do |app|
+      app.config.assets.precompile += %w(editor.css editor.js)
+    end
   end
 end
